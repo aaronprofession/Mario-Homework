cc658b2e0794ae63b48c74414f160fadf6f0e003
Aaron <aaronprofession@gmail.com>
2019-11-25 04:09:52 -0500

    Added Brick Prefab with Break animation

--- Assets/Scripts/HeadBash.cs
+++ Assets/Scripts/HeadBash.cs
@@ -4,8 +4,22 @@ using UnityEngine;
 
 public class HeadBash : MonoBehaviour
 {
+    public AudioClip[] aclips;
+    private AudioSource audio;
+    public GameObject brickAnim;
+    private void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (other.tag == "Brick")
+        {
+            audio.clip = aclips[0];
+            audio.Play();
+            Instantiate(brickAnim);
+        }
         Destroy(other.gameObject);
     }
+   
 }
---
+++ Assets/Scripts/SelfDestroy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestroy : MonoBehaviour
+{
+    
+    public void Destroy()
+    {
+        Debug.Log("Brick Destroyed");
+        Destroy(this.gameObject);
+    }
+
+}
