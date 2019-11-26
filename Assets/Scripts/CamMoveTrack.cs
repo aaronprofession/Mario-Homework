dce72f5b40185eca1491fb7fce88c6475de7474c
Aaron <aaronprofession@gmail.com>
2019-11-26 02:41:24 -0500

    Trying to fix cam track

--- Assets/Scripts/CamMoveTrack.cs
+++ Assets/Scripts/CamMoveTrack.cs
@@ -4,20 +4,42 @@ using UnityEngine;
 
 public class CamMoveTrack : MonoBehaviour
 {   //Allows mario to push the camrea when he is moving in the middle of the screen.
-    private void OnCollisionEnter2D(Collision2D other)
+    PlayerController playerscript;
+    Rigidbody2D camBody;
+    GameObject thePlayer;
+    private float speed = 10.0f;
+    private void Start()
     {
-        if(other.gameObject.tag == "Player")
-        {
-            Rigidbody2D camBody = GetComponentInParent<Rigidbody2D>();
-            camBody.isKinematic = false;
-        }
+        thePlayer = GameObject.Find("Player");
+        PlayerController playerscript = thePlayer.GetComponent<PlayerController>();
+
+        camBody = GetComponentInParent<Rigidbody2D>();
     }
-    private void OnCollisionExit2D(Collision2D other)
+    //private void OnCollisionEnter2D(Collision2D other)
+    //{
+    //    if(other.gameObject.tag == "Player")
+    //    {
+    //        Rigidbody2D camBody = GetComponentInParent<Rigidbody2D>();
+    //        camBody.isKinematic = false;
+    //    }
+    //}
+    //private void OnCollisionExit2D(Collision2D other)
+    //{
+    //    if (other.gameObject.tag == "Player")
+    //    {
+    //        Rigidbody2D camBody = GetComponentInParent<Rigidbody2D>();
+    //        camBody.isKinematic = true;
+    //    }
+    //}
+   
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag == "Player")
-        {
-            Rigidbody2D camBody = GetComponentInParent<Rigidbody2D>();
-            camBody.isKinematic = true;
-        }
+        float step = speed * Time.deltaTime;
+        Rigidbody2D pb = thePlayer.GetComponent<Rigidbody2D>();
+        
+        Vector2 playV = pb.position;
+        playV.y = camBody.position.y;
+        Vector2 camV = new Vector2(camBody.position.x, camBody.position.y);
+        Vector2.MoveTowards(transform.position, pb.position, step);
     }
 }
---
+++ Assets/Scripts/DeletionGameOver.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeletionGameOver : MonoBehaviour
+{
+    public int deaths = 0;
+    //Kills player upon touch and deletes killed Enemy prefabs
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag != "Player")
+        {
+            Destroy(other.gameObject);
+        }
+        else
+        {
+            Death();
+        }
+    }
+    void Death()
+    {
+        deaths++;
+    }
+    void GameOverCheck()
+    {
+
+    }
+
+}
